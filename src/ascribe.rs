8df9de00562a09c6762a8a5e5873fb3cd5b2077f
Nathan Sobo <nathan@zed.dev>
2024-12-28 10:14:47 -0700

    WIP

--- src/ascribe.rs
+++ src/ascribe.rs
@@ -19,9 +19,13 @@ struct Cli {
     path: String,
 }
 
-struct LsifIndex {
+struct LsifGraph {
     vertices: BTreeMap<i32, Vertex>,
     edges: BTreeMap<i32, Edge>,
+}
+
+struct LsifIndex {
+    graph: LsifGraph,
     documents: BTreeMap<i32, Document>,
 }
 
@@ -31,47 +35,21 @@ struct Document {
 
 impl LsifIndex {
     fn new(path: PathBuf) -> Result<Self> {
+        let graph = LsifGraph::from_json(&path)?;
+
         let mut this = LsifIndex {
-            vertices: BTreeMap::new(),
-            edges: BTreeMap::new(),
+            graph,
             documents: BTreeMap::new(),
         };
 
-        this.populate_graph_from_json(path)?;
         this.populate_documents();
 
         Ok(this)
     }
 
-    fn populate_graph_from_json(&mut self, path: PathBuf) -> Result<()> {
-        let line_count = BufReader::new(File::open(&path)?).lines().count();
-
-        let file = File::open(&path)?;
-        let reader = BufReader::new(file);
-
-        for (i, line) in reader.lines().enumerate() {
-            let line = line?;
-            let entry: lsif::Entry = serde_json::from_str(&line)?;
-
-            let id = parse_id(entry.id);
-            match entry.data {
-                lsif::Element::Vertex(vertex) => {
-                    self.vertices.insert(id, vertex);
-                }
-                lsif::Element::Edge(edge) => {
-                    self.edges.insert(id, edge);
-                }
-            }
-
-            print_progress(i + 1, line_count, "Parsing JSON");
-        }
-
-        Ok(())
-    }
-
     fn populate_documents(&mut self) {
-        let total = self.vertices.len();
-        for (i, (id, vertex)) in self.vertices.iter().enumerate() {
+        let total = self.graph.vertices.len();
+        for (i, (id, vertex)) in self.graph.vertices.iter().enumerate() {
             if let Vertex::Document(document) = vertex {
                 self.documents.insert(
                     *id,
@@ -86,6 +64,39 @@ impl LsifIndex {
     }
 }
 
+impl LsifGraph {
+    fn from_json(path: &Path) -> Result<Self> {
+        let mut graph = LsifGraph {
+            vertices: BTreeMap::new(),
+            edges: BTreeMap::new(),
+        };
+
+        let line_count = BufReader::new(File::open(path)?).lines().count();
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+
+        for (i, line) in reader.lines().enumerate() {
+            let line = line?;
+            let entry: lsif::Entry = serde_json::from_str(&line)?;
+
+            let id = parse_id(entry.id);
+            match entry.data {
+                lsif::Element::Vertex(vertex) => {
+                    graph.vertices.insert(id, vertex);
+                }
+                lsif::Element::Edge(edge) => {
+                    graph.edges.insert(id, edge);
+                }
+            }
+
+            print_progress(i + 1, line_count, "Parsing LSIF graph from JSON");
+        }
+        println!();
+
+        Ok(graph)
+    }
+}
+
 fn parse_id(id: NumberOrString) -> i32 {
     match id {
         NumberOrString::Number(n) => n,
@@ -97,8 +108,8 @@ fn main() -> Result<()> {
     let cli = Cli::parse();
 
     let lsif_index = LsifIndex::new(PathBuf::from(cli.path.clone()))?;
-    println!("\nNumber of vertices: {}", lsif_index.vertices.len());
-    println!("Number of edges: {}", lsif_index.edges.len());
+    println!("\nNumber of vertices: {}", lsif_index.graph.vertices.len());
+    println!("Number of edges: {}", lsif_index.graph.edges.len());
 
     Ok(())
 }
